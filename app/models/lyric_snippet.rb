f949c6c6f55b5a9ef49a78d978d6a91ca451d5de
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-11-20 16:54:01 +0100

    style: changed form inputs to match color scheme

--- app/models/lyric_snippet.rb
+++ app/models/lyric_snippet.rb
@@ -6,7 +6,7 @@ class LyricSnippet < ApplicationRecord
   validates :artist, presence: true
   validates :song, presence: true
   validates :difficulty, presence: true, inclusion: { in: 0..1000 }
-  validates :language, inclusion: { in: %w[English German] }
+  validates :language, inclusion: { in: %w[English German Spanish Russian] }
 
   def self.languages
     validators_on(:language).first.options[:in]
